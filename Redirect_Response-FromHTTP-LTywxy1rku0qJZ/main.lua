b521526f88f960899f297aa17bb278f136e2297c
admin <paul.gannon@interfaceware.com>
2016-02-23 17:23:05 +0000

    Export: More comments.

--- Redirect_Response-FromHTTP-LTywxy1rku0qJZ/main.lua
+++ Redirect_Response-FromHTTP-LTywxy1rku0qJZ/main.lua
@@ -1,4 +1,7 @@
 -- Very simple example of redirecting a web request to a different location
+-- This is helpful when creating links from one web application to another.
+-- Say you wanted to make a URL from a EMR that could redirect to a Lab system
+-- for instance.  This would be a useful tool to achieve that.
 
 function main(Data)
    local R = net.http.parseRequest{data=Data}
